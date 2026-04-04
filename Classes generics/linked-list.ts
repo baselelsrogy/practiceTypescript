94e307cddf38559be143beaf4f31136bef011999
baselelsrogy <baselhany88@gmail.com>
2026-04-04 18:25:01 +0200

    classes generic

---
+++ Classes generics/linked-list.ts
@@ -0,0 +1,88 @@
+class ListNode<T> {
+  next?: ListNode<T> | undefined;
+
+  constructor(public value: T) {}
+}
+
+class LinkedList<T> {
+  private root?: ListNode<T> | undefined;
+  private tail?: ListNode<T>;
+  private length = 0;
+
+  add(value: T) {
+    const node = new ListNode(value);
+    if (!this.root || !this.tail) {
+      this.root = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+  }
+
+  insertAt(value: T, pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let index = 0;
+      let previous = current;
+      let node = new ListNode(value);
+
+      if (pos === 0) {
+        node.next = this.root;
+        this.root = node;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        previous.next = node;
+      }
+      this.length++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  removeAt(pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current: ListNode<T> = this.root;
+      let previous: ListNode<T> = current;
+      let index = 0;
+
+      if (pos === 0) {
+        this.root = current.next;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.length--;
+      return current;
+    } else {
+      return null;
+    }
+  }
+
+  getNumberOfElement() {
+    return this.length;
+  }
+
+  print() {
+    let current = this.root;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+const numberList = new LinkedList<number>();
+numberList.add(10);
+numberList.add(23);
+numberList.add(-3);
+const nameList = new LinkedList<string>();
